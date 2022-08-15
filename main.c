7605e1a7f3afac629cd0d13de93999d5edd76222
chrisool <chrisool@ifi.uio.no>
2022-08-15 19:27:37 +0200

    Slightly changed formatting

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 
 EFI_STATUS
 EFIAPI
-efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
     InitializeLib(ImageHandle, SystemTable);
     Print(L"Hello, world!\n");
 
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo qemu-system-x86_64 -cpu qemu64 \
     -drive if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=/usr/share/OVMF/OVMF_VARS.fd \
     -net none \
-    -drive format=raw,file=uefi.img 
+    -drive format=raw,file=uefi.img
